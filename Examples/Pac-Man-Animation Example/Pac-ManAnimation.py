1ac232ff4a5a42368792ec6adc96e48535d0437f
Bhavye Mathur <bhavyemathur@gmail.com>
2020-08-02 09:50:11 +0530

    Updated the Examples to work with Version 1.0.214b19

--- Examples/Pac-Man-Animation Example/Pac-ManAnimation.py
+++ Examples/Pac-Man-Animation Example/Pac-ManAnimation.py
@@ -4,13 +4,16 @@ from goopylib.imports import *
 window = GraphWin(title="Simple Pac-Man Animation", width=600, height=200, bk_colour=DARKEST_NAVY_BLUE)
 
 # We can reference PacMan using the 'PacMan' string now as it is tagged with it
-objects = [AnimatedImage(Point(220, 100), "PacMan.png", tag="PacMan").draw()]
+pacman_frames = [f"PacMan{frame}.png" for frame in range(6)]
+objects = [AnimatedImage(Point(220, 100), pacman_frames, tag="PacMan").draw()]
 
 for index, ghost in enumerate(("Blinky", "Pinky", "Inky", "Clyde")):
     x = 300 + (index * 50)
+    ghost_frames = [f"{ghost}{frame}.png" for frame in range(6)]
 
     # The position to the draw the Animated Image & the texture file to use, then pasting the pupils of the ghost on top
-    objects.append(AnimatedImage(Point(x, 100), f"{ghost}.png").alpha_composite(Image(Point(x, 100), "Pupils.png")).draw())
+    objects.append(AnimatedImage(Point(x, 100), ghost_frames)  # Binding the frame keeps it synchronized with other Animated Images
+                   .alpha_composite(Image(Point(x, 100), "Pupils.png")).draw().bind_frame_to("PacMan"))
 
 last_update = time.time()  # The time module is automatically imported
 while True:
