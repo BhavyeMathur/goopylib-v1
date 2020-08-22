24fc2b64ff47cce3452c92c182b3ed9f9d231ac2
Bhavye Mathur <bhavyemathur@gmail.com>
2020-08-23 00:12:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
               "goopylib/sound": "goopylib/sound",
               "goopylib/physics": "goopylib/physics"},
 
-    version='1.1.20a3',
+    version='1.1.28a4',
     license='MIT License',
 
     description='A simple-yet-powerful 2D graphics framework built on top of Tkinter capable of creating good-looking, modern GUIs, games, and animations.',
@@ -25,7 +25,7 @@ setup(
     author='Bhavye Mathur',
     author_email='bhavyemathur@gmail.com',
     url='https://github.com/BhavyeMathur/goopylib',
-    download_url='https://github.com/BhavyeMathur/goopylib/archive/v1.1.20-alpha.tar.gz',
+    download_url='https://github.com/BhavyeMathur/goopylib/archive/v1.1.28-alpha.tar.gz',
     keywords=['Tkinter', '2D Graphics', 'Python GUI', 'Game Creator', 'Graphics Library'],
     install_requires=['pillow', 'numpy'],
     project_urls={"Bug Tracker": "https://github.com/BhavyeMathur/goopylib/issues",
