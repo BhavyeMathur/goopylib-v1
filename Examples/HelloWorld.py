53c6adefd5504a000f258826915bc011fb1df368
Bhavye Mathur <bhavyemathur@gmail.com>
2020-08-23 00:18:01 +0530

    Fixed Text Alignment in Example

--- Examples/HelloWorld.py
+++ Examples/HelloWorld.py
@@ -5,11 +5,13 @@ window = Window(title="Hello World!", width=500, height=500, bk_colour=DARKEST_G
 Rectangle(Point(0, 220), Point(500, 280), fill=DARK_GREY, outline_width=0).draw()  # Drawing a rectangle in the back
 
 # Creates the title text and makes it glide in from the side with an elastic ease for 2 seconds
-title = Text(Point(0, 250), "Hello World!", font_colour=WHITE, font_size=30, font_face="Century Gothic", font_style="bold").draw()
+title = Text(Point(0, 250), "Hello World!", text_align="center", font_colour=WHITE, font_size=30,
+             font_face="Century Gothic", font_style="bold").draw()
 title.glide_to_x(250, time=2, easing=ease_elastic_out())
 
 # Creates the subtitle and makes it glide in from the bottom with a circle ease for 2 seconds
-subtitle = Text(Point(250, 520), "Bhavye's 2nd program", font_colour=WHITE, font_size=12, font_face="Century Gothic").draw()
+subtitle = Text(Point(250, 520), "Bhavye's 2nd program", text_align="center", font_colour=WHITE, font_size=12,
+                font_face="Century Gothic").draw()
 subtitle.glide(0, 0, time=1)
 subtitle.glide_to_y(300, time=2, easing=ease_circle_out())
 
