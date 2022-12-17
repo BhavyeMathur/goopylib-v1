816a71742294eafe783eb531ce421cb599875c6a
Bhavye Mathur <bhavyemathur@gmail.com>
2022-12-17 23:16:42 +0530

    Warning-less disabled log statements (on CLion)

--- goopylib/src/goopylib/Debug/Log.h
+++ goopylib/src/goopylib/Debug/Log.h
@@ -41,9 +41,9 @@ template<typename OStream, typename T, glm::qualifier Q>
 #define GP_PY_CRITICAL(...) gp::Log::getPythonLogger()->critical(__VA_ARGS__)
 #define GP_CRITICAL(...)      gp::Log::ClientLogger()->critical(__VA_ARGS__)
 #else
-#define GP_CORE_CRITICAL(...)
-#define GP_PY_CRITICAL(...)
-#define GP_CRITICAL(...)
+#define GP_CORE_CRITICAL(...) nullptr
+#define GP_PY_CRITICAL(...) nullptr
+#define GP_CRITICAL(...) nullptr
 #endif
 
 #if (GP_LOGGING >= 2)
@@ -51,9 +51,9 @@ template<typename OStream, typename T, glm::qualifier Q>
 #define GP_PY_ERROR(...)    gp::Log::getPythonLogger()->error(__VA_ARGS__)
 #define GP_ERROR(...)         gp::Log::getClientLogger()->error(__VA_ARGS__)
 #else
-#define GP_CORE_ERROR(...)
-#define GP_PY_ERROR(...)
-#define GP_ERROR(...)
+#define GP_CORE_ERROR(...) nullptr
+#define GP_PY_ERROR(...) nullptr
+#define GP_ERROR(...) nullptr
 #endif
 
 #if (GP_LOGGING >= 3)
@@ -61,9 +61,9 @@ template<typename OStream, typename T, glm::qualifier Q>
 #define GP_PY_WARN(...)     gp::Log::getPythonLogger()->warn(__VA_ARGS__)
 #define GP_WARN(...)          gp::Log::getClientLogger()->warn(__VA_ARGS__)
 #else
-#define GP_CORE_WARN(...)
-#define GP_PY_WARN(...)
-#define GP_WARN(...)
+#define GP_CORE_WARN(...) nullptr
+#define GP_PY_WARN(...) nullptr
+#define GP_WARN(...) nullptr
 #endif
 
 #if (GP_LOGGING >= 4)
@@ -71,9 +71,9 @@ template<typename OStream, typename T, glm::qualifier Q>
 #define GP_PY_INFO(...)     gp::Log::getPythonLogger()->info(__VA_ARGS__)
 #define GP_INFO(...)          gp::Log::getClientLogger()->info(__VA_ARGS__)
 #else
-#define GP_CORE_INFO(...)
-#define GP_PY_INFO(...)
-#define GP_INFO(...)
+#define GP_CORE_INFO(...) nullptr
+#define GP_PY_INFO(...) nullptr
+#define GP_INFO(...) nullptr
 #endif
 
 #if (GP_LOGGING >= 5)
@@ -81,9 +81,9 @@ template<typename OStream, typename T, glm::qualifier Q>
 #define GP_PY_DEBUG(...)    gp::Log::getPythonLogger()->debug(__VA_ARGS__)
 #define GP_DEBUG(...)         gp::Log::getClientLogger()->debug(__VA_ARGS__)
 #else
-#define GP_CORE_DEBUG(...)
-#define GP_PY_DEBUG(...)
-#define GP_DEBUG(...)
+#define GP_CORE_DEBUG(...) nullptr
+#define GP_PY_DEBUG(...) nullptr
+#define GP_DEBUG(...) nullptr
 #endif
 
 #if (GP_LOGGING >= 6)
@@ -91,7 +91,7 @@ template<typename OStream, typename T, glm::qualifier Q>
 #define GP_PY_TRACE(...)    gp::Log::getPythonLogger()->trace(__VA_ARGS__)
 #define GP_TRACE(...)         gp::Log::getClientLogger()->trace(__VA_ARGS__)
 #else
-#define GP_CORE_TRACE(...)
-#define GP_PY_TRACE(...)
-#define GP_TRACE(...)
+#define GP_CORE_TRACE(...) nullptr
+#define GP_PY_TRACE(...) nullptr
+#define GP_TRACE(...) nullptr
 #endif
