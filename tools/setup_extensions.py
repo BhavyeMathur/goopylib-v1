a301b8b86818c7add41241eb2f5107b1df63f1d2
Bhavye Mathur <bhavyemathur@gmail.com>
2023-01-07 11:03:13 +0530

    Moved setup_extensions into tools/

--- tools/setup_extensions.py
+++ tools/setup_extensions.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from setuptools import setup
+import setuptools
 
 from setup import find_extensions, check_version, RUNTIME_LIBRARY_DIRS
 
@@ -11,7 +11,7 @@ if sys.platform == "darwin":
     RUNTIME_LIBRARY_DIRS.append(f"{os.getcwd()}/binaries/lib-macos")
 
 if len(sys.argv) == 1:
-    os.system("python setup_extensions.py build")
+    os.system("python tools/setup_extensions.py build")
 else:
-    setup(ext_modules=find_extensions(),
-          options={"build": {"build_lib": "."}})
+    setuptools.setup(ext_modules=find_extensions(),
+                     options={"build": {"build_lib": "."}})
