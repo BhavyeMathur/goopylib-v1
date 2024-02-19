03f51f0daa40e00e80b3906aed14a48da9323d52
Bhavye Mathur <bhavyemathur@gmail.com>
2024-02-19 19:44:44 +0530

    Added error checking & logging to UniformBuffer.cpp
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

--- src/core/UniformBuffer.cpp
+++ src/core/UniformBuffer.cpp
@@ -1,13 +1,22 @@
 #include "UniformBuffer.h"
+#include "debug/Error.h"
+
+#include <GLFW/glfw3.h>
 #include <opengl.h>
 
 namespace gp {
     UniformBuffer::UniformBuffer(const BufferLayout &layout)
-        : Buffer(0, layout) {
+        : Buffer{0, layout} {
+        GP_CORE_INFO("gp::UniformBuffer::UniformBuffer({0})", m_RendererID);
     }
 
-    void UniformBuffer::setBinding(uint32_t binding) const {
+    void UniformBuffer::setBinding(const uint32_t binding) const {
+        GP_CORE_INFO("gp::UniformBuffer::setBinding({0}, binding={1})", m_RendererID, binding);
+        GP_CHECK_ACTIVE_CONTEXT("gp::UniformBuffer::setBinding()");
+        GP_CHECK_RENDERER_ID("gp::UniformBuffer::setBinding()");
+
         bind();
+        GP_OPENGL("glBindBufferBase(GL_UNIFORM_BUFFER, index={0}, buffer={1})", binding, m_RendererID);
         glBindBufferBase(GL_UNIFORM_BUFFER, binding, m_RendererID);
     }
 
